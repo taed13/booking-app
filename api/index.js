b48e9c61fe08e647345c08b9cdff35b38940104a
taed13 <taedtech13@gmail.com>
2023-08-08 18:01:08 +0700

    done upload by link

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 const { json } = require('express/lib/response.js');
 require('dotenv').config();
 const app = express();
@@ -14,6 +15,7 @@ const jwtSecret = 'asdfadsf123132asfdasaaqwerqer';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
@@ -81,6 +83,16 @@ app.get('/profile', function (req, res) {
 
 app.post('/logout', (req, res) => {
     res.cookie('token', "").json(true);
+});
+
+app.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/',
+    });
+    res.json(newName);
 });
 
 app.listen(4000);
