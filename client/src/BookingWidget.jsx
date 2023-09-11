f272772a56f34d2a3e48463e84842c0531e6927f
taed13 <taedtech13@gmail.com>
2023-09-11 15:22:55 +0700

    booking functionality

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import { differenceInCalendarDays } from "date-fns";
+
 export default function BookingWidget({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [redirect, setRedirect] = useState("");
+  let numberOfNights = 0;
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
+    );
+  }
+
+  async function bookThisPlace() {
+    const response = await axios.post("/bookings", {
+      checkIn,
+      checkOut,
+      numberOfGuests,
+      name,
+      phone,
+      place: place._id,
+      price: numberOfNights * place.price,
+    });
+    const bookingId = response.data._id;
+
+    setRedirect(`/account/bookings/${bookingId}`);
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
   return (
     <>
       <div className="bg-white shadow p-4 rounded-2xl">
@@ -9,19 +45,50 @@ export default function BookingWidget({ place }) {
           <div className="flex">
             <div className="px-4 py-3">
               <label>Check in:</label>
-              <input type="date" />
+              <input
+                type="date"
+                value={checkIn}
+                onChange={(ev) => setCheckIn(ev.target.value)}
+              />
             </div>
             <div className="px-4 py-3 border-l">
               <label>Check in:</label>
-              <input type="date" />
+              <input
+                type="date"
+                value={checkOut}
+                onChange={(ev) => setCheckOut(ev.target.value)}
+              />
             </div>
           </div>
           <div className="px-4 py-3 border-l">
             <label>Number of guests:</label>
-            <input type="number" value={1} />
+            <input
+              type="number"
+              value={numberOfGuests}
+              onChange={(ev) => setNumberOfGuests(ev.target.value)}
+            />
           </div>
+          {numberOfNights > 0 && (
+            <div className="py-3 px-4 border-t">
+              <label>Your full name:</label>
+              <input
+                type="number"
+                value={name}
+                onChange={(ev) => setName(ev.target.value)}
+              />
+              <label>Phone number:</label>
+              <input
+                type="tel"
+                value={phone}
+                onChange={(ev) => setPhone(ev.target.value)}
+              />
+            </div>
+          )}
         </div>
-        <button className="primary mt-4">Book this place</button>
+        <button onClick={bookThisPlace} className="primary mt-4">
+          Book this place
+          {numberOfNights > 0 && <span>${numberOfNights * place.price}</span>}
+        </button>
       </div>
     </>
   );
---
+++ client/src/pages/BookingsPage.jsx
@@ -0,0 +1,7 @@
+export default function BookingPage() {
+  return (
+    <>
+      <div>my bookings here</div>
+    </>
+  );
+}
